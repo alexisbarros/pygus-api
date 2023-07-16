4abbaf2a9b09fabbb9c2e1acf25c517ffc06826a
Alexis Barros <apenasalexis@gmail.com>
2023-07-16 18:44:20 -0300

    feature: cloud storage

---
+++ services/cloud-storage.service.js
@@ -0,0 +1,29 @@
+const { Storage } = require('@google-cloud/storage');
+
+const storage = new Storage({
+    keyFilename: process.env.GOOGLE_CLOUD_STORAGE_FILE_KEY,
+    projectId: 'pygusapp',
+});
+const bucketName = 'pygus-storage';
+
+exports.getFileUrl = async (directoryPath, fileName) => {
+  try {
+
+    const bucket = storage.bucket(bucketName);
+    const file = bucket.file(`${directoryPath}/${fileName}`);
+    const fileExists = await file.exists();
+
+    if (!fileExists[0]) {
+      return '';
+    }
+
+    const [url] = await file.getSignedUrl({
+      action: 'read',
+      expires: Date.now() + 15 * 60 * 1000, // URL expira em 15 minutos
+    });
+
+    return url;
+  } catch (error) {
+    console.error('Erro ao obter a URL do arquivo:', error);
+  }
+};
