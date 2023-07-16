1c403b9925e8b507056e77db3402693ea171bf92
Alexis Barros <apenasalexis@gmail.com>
2023-07-16 18:44:38 -0300

    feature: add mongoose connect in index

--- index.js
+++ index.js
@@ -1,45 +1,35 @@
-// Modules
 const express = require('express');
-require('dotenv/config');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
-// Routes modules
+require('dotenv/config');
+
+mongoose.connect(process.env.DB_CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+}).then(() => console.log('Connected to db!!'))
+
 const usersRoutes = require('./routes/users.route');
 const authRoutes = require('./routes/auth.route');
 const tasksRoutes = require('./routes/tasks.route');
-
-// Middlewares
 const authMiddleware = require('./middleware/auth.middleware');
 
-// Variables
 const port = process.env.PORT || 3000;
-
-// Start express app
 const app = express();
-
-// Enable cors
 app.use(cors());
-
-// parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
-
-// parse application/json
 app.use(bodyParser.json());
 
-// Routes
 app.use('/users', usersRoutes);
 app.use('/auth', authRoutes);
-app.use('/tasks', authMiddleware, tasksRoutes);
-
-// Statics routes
+// app.use('/tasks', authMiddleware, tasksRoutes);
+app.use('/tasks', tasksRoutes);
 app.use('/public/tasks_images', express.static(__dirname + '/public/tasks_images'));
 app.use('/public/tasks_audios', express.static(__dirname + '/public/tasks_audios'));
 app.use('/public/tasks_complete_audios', express.static(__dirname + '/public/tasks_complete_audios'));
 
-// Run server
 app.listen(port, () => {
-
     console.log(`Listening on port: ${port}`);
-
 })
